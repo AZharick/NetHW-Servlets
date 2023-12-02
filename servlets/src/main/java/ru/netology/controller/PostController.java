75d110529ef30b773824aafc45fef56722f22d47
AZharick <bopohlabs@mail.ru>
2023-12-02 20:36:44 +0300

    C3: cntrlr add

--- servlets/src/main/java/ru/netology/controller/PostController.java
+++ servlets/src/main/java/ru/netology/controller/PostController.java
@@ -1,9 +1,7 @@
 package ru.netology.controller;
-
 import com.google.gson.Gson;
 import ru.netology.model.Post;
 import ru.netology.service.PostService;
-
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.Reader;
@@ -11,7 +9,6 @@ import java.io.Reader;
 public class PostController {
   public static final String APPLICATION_JSON = "application/json";
   private final PostService service;
-
   public PostController(PostService service) {
     this.service = service;
   }
@@ -23,8 +20,11 @@ public class PostController {
     response.getWriter().print(gson.toJson(data));
   }
 
-  public void getById(long id, HttpServletResponse response) {
-    // TODO: deserialize request & serialize response
+  public void getById(long id, HttpServletResponse response) throws IOException {
+    response.setContentType(APPLICATION_JSON);
+    final var data = service.getById(id);
+    final var gson = new Gson();
+    response.getWriter().print(gson.toJson(data));
   }
 
   public void save(Reader body, HttpServletResponse response) throws IOException {
@@ -35,7 +35,9 @@ public class PostController {
     response.getWriter().print(gson.toJson(data));
   }
 
-  public void removeById(long id, HttpServletResponse response) {
-    // TODO: deserialize request & serialize response
+  public void removeById(long id, HttpServletResponse response) throws IOException {
+    response.setContentType(APPLICATION_JSON);
+    service.removeById(id);
+    response.getWriter().print("Element deleted");
   }
 }
--- servlets/src/main/java/ru/netology/repository/PostRepository.java
+++ servlets/src/main/java/ru/netology/repository/PostRepository.java
@@ -1,52 +1,56 @@
 package ru.netology.repository;
 
-import ru.netology.exception.NotFoundException;
 import ru.netology.model.Post;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.NoSuchElementException;
 import java.util.Optional;
-import java.util.concurrent.CopyOnWriteArrayList;
-import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicLong;
 
 // Ex-Stub
 public class PostRepository {
-   private CopyOnWriteArrayList<Post> postsList = new CopyOnWriteArrayList<>();
-   private AtomicInteger postsListCounter = new AtomicInteger(0);
+   protected ConcurrentHashMap<AtomicLong, String> posts = new ConcurrentHashMap<>();
+   protected AtomicLong postsListCounter = new AtomicLong(0);
 
    public List<Post> all() {
-      return postsList;
+      ArrayList<Post> allPosts = new ArrayList<>();
+      for (int i = 1; i < posts.size(); i++) {
+         Post post = new Post(i, posts.get(i));
+         allPosts.add(post);
+      }
+      return allPosts;
    }
 
    public Optional<Post> getById(long id) {
-      return Optional.ofNullable(postsList.get((int) id));
+      Post post = null;
+      if (posts.containsKey(id)) {
+         post = new Post(id, posts.get(id));
+      }
+         return Optional.ofNullable(post);
    }
 
-   public Post save(Post post) throws NotFoundException {
-      boolean isUpdated = false;
+   public Post save(Post post){
+      Post updatedPost;
       if (post.getId() == 0) {
-         postsList.add(post);
          postsListCounter.getAndIncrement();
+         posts.put(postsListCounter, post.getContent());
+         updatedPost = new Post(postsListCounter.longValue(), post.getContent());
       } else {
-         for (int i = 0; i < postsList.size(); i++) {
-            if (post.getId() == postsList.get(i).getId()) {
-               postsList.get(i).setContent(post.getContent());
-               isUpdated = true;
-            }
-
-            if ((i == postsList.size()) && !isUpdated) {
-               throw new NotFoundException("No post with such ID found!");
-            }
-         }
+         AtomicLong id = new AtomicLong(post.getId());
+         posts.put(id, post.getContent());
+         updatedPost = new Post(id.longValue(), post.getContent());
       }
-      return post;
+      return updatedPost;
    }
 
-   public void removeById(long id) {
-      for (Post post: postsList) {
-         if (post.getId() == id) {
-            postsList.remove(id);
-            postsListCounter.getAndDecrement();
-         }
+   public void removeById(long id) throws NoSuchElementException {
+      if (this.getById(id).equals(Optional.empty())) {
+         throw new NoSuchElementException("No such ID!");
       }
+      AtomicLong atomicId = new AtomicLong(id);
+      posts.remove(atomicId);
+      postsListCounter.getAndDecrement();
    }
 }
